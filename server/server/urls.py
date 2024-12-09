447c11e2545ab1f83db0fa0006ed702306a3574c
kalyanakannan <kalyanakannan.p@gmail.com>
2024-12-09 11:42:13 +0530

    fix(routing): Separate API routes and React app routes
    - Explicitly included `/api/` routes for Django API endpoints in `urls.py`.
    - Added a catch-all route to serve React's `index.html` for all non-API routes.
    - Ensures React handles client-side routing while Django serves API requests.

--- server/server/urls.py
+++ server/server/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 from rest_framework_simplejwt.views import (
@@ -30,6 +30,6 @@ urlpatterns = [
     path('api/', include('meetings.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-     path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
 
